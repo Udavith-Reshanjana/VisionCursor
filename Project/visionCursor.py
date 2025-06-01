c160c181cb4111a1a6c3948246f137ac389dbdb9
Udavith Reshanjana <146712079+Udavith-Reshanjana@users.noreply.github.com>
2025-06-01 12:52:39 +0530

    bug solved
    only recognize the left hand, not left or right

--- Project/visionCursor.py
+++ Project/visionCursor.py
@@ -31,29 +31,39 @@ while True:
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(img_rgb)
 
-    if results.multi_hand_landmarks:
+    if results.multi_hand_landmarks and results.multi_handedness:
         hand_landmarks = results.multi_hand_landmarks[0]
+        hand_label = results.multi_handedness[0].classification[0].label  # 'Left' or 'Right'
+
         lm_list = []
+        h, w = img.shape[:2]
         for id, lm in enumerate(hand_landmarks.landmark):
-            h, w = img.shape[:2]
             cx, cy = int(lm.x * w), int(lm.y * h)
             lm_list.append((id, cx, cy))
 
         if lm_list:
-            # Move cursor with index finger
-            _, x, y = lm_list[8]
+            # Move mouse with index finger
+            x, y = lm_list[8][1], lm_list[8][2]
             screen_x = int(x * screen_width / w)
             screen_y = int(y * screen_height / h)
             pyautogui.moveTo(screen_x, screen_y)
 
-            # Finger detection
+            # Detect fingers
             fingers = []
-            fingers.append(lm_list[4][1] > lm_list[3][1])  # Thumb
+
+            # Thumb
+            if hand_label == "Right":
+                fingers.append(lm_list[4][1] > lm_list[3][1])  # Thumb right of joint
+            else:  # Left
+                fingers.append(lm_list[4][1] < lm_list[3][1])  # Thumb left of joint
+
+            # Other fingers
             for i in range(1, 5):
                 fingers.append(lm_list[tip_ids[i]][2] < lm_list[tip_ids[i] - 2][2])
+
             total_fingers = fingers.count(True)
 
-            # Left click (5 fingers)
+            # Left click (5 fingers up)
             if total_fingers == 5:
                 if not clicked:
                     pyautogui.click()
@@ -61,8 +71,8 @@ while True:
             else:
                 clicked = False
 
-            # Drag (2 fingers: index + middle)
-            if total_fingers == 2 and fingers[1] and fingers[2]:
+            # Drag (index + middle)
+            if fingers[1] and fingers[2] and total_fingers == 2:
                 if not dragging:
                     pyautogui.mouseDown()
                     dragging = True
@@ -71,8 +81,8 @@ while True:
                     pyautogui.mouseUp()
                     dragging = False
 
-            # Right click (3 fingers: index + middle + ring)
-            if total_fingers == 3 and fingers[1] and fingers[2] and fingers[3]:
+            # Right click (index + middle + ring)
+            if fingers[1] and fingers[2] and fingers[3] and total_fingers == 3:
                 if not right_clicked:
                     pyautogui.rightClick()
                     right_clicked = True
@@ -83,7 +93,7 @@ while True:
         mp_draw.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
     cv2.imshow("Gesture Mouse Control", img)
-    if cv2.waitKey(1) & 0xFF == 27: # Escape key to exit
+    if cv2.waitKey(1) & 0xFF == 27:
         break
 
 cap.release()
